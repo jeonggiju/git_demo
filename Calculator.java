35efdb76c3264249c4dbf34f05dc6e1e57878f2d
jeonggiju <halogiju123@gmail.com>
2025-07-04 10:03:57 +0900

    feat: modify calculator2

--- Calculator.java
+++ Calculator.java
@@ -1,8 +1,8 @@
 public class Calculator  {
     public int add(String str1, String str2) {
-        int a = Integer.parseInt(str1);
-        int b = Integer.parseInt(str2);
-        return a+b;
+        int aa = Integer.parseInt(str1);
+        int bb = Integer.parseInt(str2);
+        return aa+bb;
 
     }
 }
